06e16d7a7a1b7eb4474b21fc23312541dfd80287
daniellien2018 <daniellien2018@gmail.com>
2022-06-27 17:21:33 -0700

    first commit

---
+++ Ghost.rb
@@ -0,0 +1,17 @@
+require "dictionary.txt"
+
+class Game
+
+    attr_reader :@player 
+    def initialize
+        @player
+        @fragment
+        @dictionary = {}
+    end
+    def player
+        @player
+    end
+    def fragment
+        @fragment
+    end
+end
